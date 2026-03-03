9c879b019677b12d66e79defffea97c94415715a
Alex <Alex@DESKTOP-11PQO4G>
2026-03-03 18:07:43 -0300

    (feat: API) Criação do projeto, instalação das bibliotecas swagger e scalar

--- DeslandesApp.API/Program.cs
+++ DeslandesApp.API/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+//Swagger 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,6 +17,12 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+//Swagger 
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//Scalar 
+app.MapScalarApiReference(s => s.WithTheme(ScalarTheme.BluePlanet));
 
 app.UseAuthorization();
 
